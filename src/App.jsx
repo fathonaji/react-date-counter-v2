5d48155e8c9ca112b116a17326c36ad334a483f1
fathonaji <fathona21@gmail.com>
2024-01-29 22:03:43 +0700

    create increase decrease count by number and if the input text field is changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function App() {
   return (
     <div className="center">
@@ -7,17 +9,53 @@ function App() {
 }
 
 function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  function increaseStep() {
+    setStep(step + 1);
+  }
+  function decreaseStep() {
+    if (step > 1) setStep(step - 1);
+  }
+  function increaseCount() {
+    setCount(count + step);
+  }
+  function decreaseCount() {
+    setCount(count - step);
+  }
+  function adjustCount(value) {
+    setCount(isNaN(value) ? 0 : Number(value));
+  }
   return (
     <>
       <div className="d-flex">
         <input type="range" min="0" max="10" />
       </div>
       <div className="d-flex">
-        <button>-</button>
-        <input type="text" value="0" />
-        <button>+</button>
+        <button onClick={decreaseCount}>-</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => adjustCount(e.target.value)}
+        />
+        <button onClick={increaseCount}>+</button>
       </div>
-      <div className="d-flex mt-1">1 days from today is x</div>
+      <div className="d-flex mt-1">
+        {count < 0 && (
+          <div>
+            {Math.abs(count)} days ago was {date.toDateString()}
+          </div>
+        )}
+        {count == 0 && <div>Today is {date.toDateString()}</div>}
+        {count > 0 && (
+          <div>
+            {count} days from today is {date.toDateString()}
+          </div>
+        )}
+      </div>
       <div className="d-flex mt-1">
         <button>Reset</button>
       </div>

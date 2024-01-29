df841eb09db71a1a5497d0f3873f593a5698ad4c
fathonaji <fathona21@gmail.com>
2024-01-29 21:50:27 +0700

    setup ui for date counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="center">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  return (
+    <>
+      <div className="d-flex">
+        <input type="range" min="0" max="10" />
+      </div>
+      <div className="d-flex">
+        <button>-</button>
+        <input type="text" value="0" />
+        <button>+</button>
+      </div>
+      <div className="d-flex mt-1">1 days from today is x</div>
+      <div className="d-flex mt-1">
+        <button>Reset</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
